61f581f175d61d3d7ca8166405f4d347d7083166
Os-Carlos <78707106+Os-Carlos@users.noreply.github.com>
2023-09-12 14:46:35 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:8081"
+    origin: "https://tutorials-clientes.vercel.app/tutorials"
 };
 
 app.use(cors(corsOptions));
