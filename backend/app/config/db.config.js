4694bf4949a4370771a02e17ad9aaaffb030dbcf
Os-Carlos <78707106+Os-Carlos@users.noreply.github.com>
2023-09-19 14:09:53 -0600

    Update db.config.js

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: "ep-dark-queen-11405827-pooler.us-east-1.postgres.vercel-storage.com",
     USER: "default",
     PASSWORD: "kMxGY7Vgz0Kf",
-    DB: "verceldb",
+    DB: "tutorials",
     PORT: 5432,
     dialect: "postgres",
     pool: {
